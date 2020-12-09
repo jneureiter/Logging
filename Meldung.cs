1ecc11d9f823ef407daed7c848dd9149a7e56dca
Josef Neureiter <josef.neu05@gmail.com>
2020-12-09 09:46:39 +0100

    meldungen wird zur Liste hinzugefügt

---
+++ Meldung.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logging
+{
+    public class Meldung
+    {
+        public string FDescription { get; set; }
+
+        public string FDate { get; set; }
+
+        public string FSeverity  { get; set; }
+    }
+}
