58702701dba7cbed64dbd397f9c8b826d14828f8
Josef Neureiter <josef.neu05@gmail.com>
2020-12-09 09:50:13 +0100

    Meldungen werden ausgegeben

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Logging
 {
@@ -6,7 +7,30 @@ namespace Logging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Meldung> meldungen = new List<Meldung>();
+
+            Meldung eins = new Meldung();
+            eins.FDescription = "Dateizugriff verweigert";
+            eins.FDate = "2020-12-18";
+            eins.FDate = "Fataler Fehler";
+
+            meldungen.Add(eins);
+
+            Meldung zwei = new Meldung();
+            zwei.FDescription = "Speicherplatz zu knapp - die Datei konnte nicht erstellt werden";
+            zwei.FDate = "2020-12-07";
+            zwei.FSeverity = "Fataler Fehler";
+
+            meldungen.Add(zwei);
+
+            foreach (var item in meldungen)
+            {
+                Console.WriteLine($" {item.FSeverity}: {item.FDate}     {item.FDescription}");
+            }
+
+
+
+
         }
     }
 }
